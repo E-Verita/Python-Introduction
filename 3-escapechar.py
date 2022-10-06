98b960285595d92054a0aef60731476baad2cf1e
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-10-07 00:21:40 +0300

    str, escape characters, operators

---
+++ 3-escapechar.py
@@ -0,0 +1,28 @@
+splitString = 'This string has been \nsplit over \nseveral \nlines'
+print(splitString)
+
+tabbedString = '1\t2\t3\t4\t5'
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\' uh,... he\'s resting"')
+#or
+print("The pet shop owner said \"No, no, 'e' uh,... he's resting\"")
+print("""The pet shop owner said "No, no, 'e' uh,... he's resting". """)
+
+anotherSplitString = """This string has been \
+split \
+over \
+several \
+lines"""
+print(anotherSplitString)
+
+anotherIsSplitString = """This string has been 
+split 
+over 
+several 
+lines"""
+print(anotherIsSplitString)
+
+print("C:\\Users\\tim\\notes.txt")
+print(r"C:\Users\tim\notes.txt")
+
---
+++ fast.py
@@ -0,0 +1,6 @@
+vecums =19
+print("Mans vecums ir ", vecums)
+
+mansvecums = 'aizvēsturisks'
+print("Mans vecums ir " + mansvecums)
+
