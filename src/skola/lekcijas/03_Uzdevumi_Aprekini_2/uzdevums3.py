1d4149c4e57ca260d375ca45520293f7127c59c0
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-10-12 23:23:47 +0300

    03_Uzdevumi_Aprekini_2

--- src/skola/lekcijas/03_Uzdevumi_Aprekini_2/uzdevums3.py
+++ src/skola/lekcijas/03_Uzdevumi_Aprekini_2/uzdevums3.py
@@ -1,6 +1,5 @@
 # Uzrakstīt Python skriptu, kas nolasa reālu skaitli un izvada pirmo ciparu, kas atrodas pa kreisi
 # no decimālās zīmes, un pirmo ciparu, kas atrodas pa labi no decimālās zīmes.
-import math
 
 x = float(input("Ievadi reālu skaitli ===> "))
 
