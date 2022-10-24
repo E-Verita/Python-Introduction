9092f7c38cf79fca4655ec7520a4b156df254099
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-10-24 23:37:07 +0300

    For loops, ranges

---
+++ src/udemy/13 - ranges.py
@@ -0,0 +1,11 @@
+for i in range(1, 20):
+    print(f"for i in range(1, 20): i = {i}")
+
+for i in range(10):
+    print(f"for i in range(10) i={i}")
+
+for i in range(0, 10, 2):
+    print(f"for i in range(0, 10, 2): i={i}")
+
+for i in range(10, 0, -2):
+    print(f"for i in range (10, 0, -2): i={i}")
