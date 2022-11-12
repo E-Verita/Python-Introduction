0f17000fc245098b90a10b04137231269b07e250
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-11-12 15:04:18 +0200

    List comprehension exercise -  You are given three integers  and  representing the dimensions of a cuboid along with an integer .Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i +j +k  is not equal to n. Here 0 <= i <= x; 0 <= j <= y; 0 <= k <= z;

--- src/other/list_comprehension.py
+++ src/other/list_comprehension.py
@@ -1,6 +1,76 @@
 # LIST COMPREHENSIONS
+# Let's learn about list comprehensions! You are given three integers  and  representing the dimensions of a cuboid along with an integer .
+# Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i +j +k  is not equal to n.
+# Here 0 <= i <= x; 0 <= j <= y; 0 <= k <= z;  Please use list comprehensions rather than multiple loops, as a learning exercise.
+""""
+Sample Input 0
+1
+1
+1
+2
+Sample Output 0
+[[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
 
-# standard method:
+Sample Input 1
+2
+2
+2
+2
+Sample Output 1
+[[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 2], [0, 2, 1], [0, 2, 2], [1, 0, 0], [1, 0, 2], [1, 1, 1], [1, 1, 2], [1, 2, 0], [1, 2, 1], [1, 2, 2], [2, 0, 1], [2, 0, 2], [2, 1, 0], [2, 1, 1], [2, 1, 2], [2, 2, 0], [2, 2, 1], [2, 2, 2]]
+
+"""
+
+x = int(input("x ===> "))
+y = int(input("y ===> "))
+z = int(input("z ===> "))
+n = int(input("n ===> "))
+
+permutations = []
+
+for i in range(0, x+1):
+    for j in range(0, y + 1):
+        for k in range (0, z + 1):
+            if i + j + k != n:
+                xlist =[i, j, k]
+                print(xlist)
+                permutations.append(xlist)
+print(permutations)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("\n\n\n")
 my_list = [1, 2, 3]
 new_list = []
 for item in my_list:
