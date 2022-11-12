f6f06cc4dd2625e1507b2b11bc5f227cb6aa4e24
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-11-13 00:46:33 +0200

    while loops

--- src/udemy/9 - if, elif, else.py
+++ src/udemy/9 - if, elif, else.py
@@ -1,20 +1,22 @@
-answer = 5
-print("Please guess the number between 1 and 10: ")
-guess = int(input())
-
-if guess == answer:
-    print("You got it first time!")
-
-else:
+import random
+highest = 1000
+lowest = 1
+answer = random.randint(lowest, highest)
+print(f"answer: {answer}") #TODO: Remove after testing
+print(f"Please guess the number between {lowest} and {highest}: ")
+guess = 0
+while guess != answer:
+    guess = int(input())
+    if guess == 0:
+        print("Game terminated!")
+        break
     if guess < answer:
         print("Please guess higher!")
-    else:
+    elif guess > answer:
         print("Please guess lower!")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it!")
-    else:
-        print("Sorry, you did not guessed correctly")
+else:
+    print("You got it!")
+
 
 # if guess < answer:
 #     print("Please guess higher!")
