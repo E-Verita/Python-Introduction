93f681092ca9bfcaf3723880a0b9b6a1e38151f4
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-11-17 13:26:16 +0200

    while loops - choice

--- src/udemy/17 - else in a loop.py
+++ src/udemy/17 - else in a loop.py
@@ -1,21 +1,19 @@
-choice = int(input("Please choose your option from list bellow:"
-      "\n1. Learn Python"
-      "\n2. Learn Java"
-      "\n3. Go swimming"
-      "\n4. Play chess"
-      "\n5. Read a book"
-      "\n0. Exit"))
-
-while choice != 0:
-    if choice == 1:
-        print("1")
+choice = "-"
+while choice != "0":
+    if choice in "12345":
+        print(f"You choose {choice}")
+    else:
+        print("Please choose your option from list bellow:"
+              "\n1. Learn Python"
+              "\n2. Learn Java"
+              "\n3. Go swimming"
+              "\n4. Play chess"
+              "\n5. Read a book"
+              "\n0. Exit")
     choice = input()
 
 
-
-
-
-
+print("*"*20)
 numbers = [1, 45, 30, 12, 60]
 
 for number in numbers:
