e40364af5fab64536b150578a164ea4fb64656df
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-11-21 23:59:08 +0200

    while loops - choice

---
+++ src/udemy/18 - sequence types.py
@@ -0,0 +1,23 @@
+"""
+Basic sequence types - list, tuple, range
+
+Also binary data and text strings
+Sequence - ordered collection of items.
+
+Iterables - anything you can iterate over
+Indexing starts from zero
+
+All sequences can be iterated over.
+Not all iterables are sequences.
+
+STRINGS - immutable (can't be changed)
+
+LISTS - mutable (can be changed)
+
+IMMUTABLE:
+int float bool str touple frozenset byte
+
+MUTABLE:
+list dict set Bytearray
+
+"""
---
+++ src/udemy/19 - lists.py
@@ -0,0 +1,12 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse"]
+
+for part in computer_parts:
+    print(part)
+print()
+print(computer_parts[0])
+print(computer_parts[0:3])
+print()
+print(computer_parts[-1])
