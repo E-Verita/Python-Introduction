3068e54bb54321207f42e151a71e55dd4aeb80b4
E-Verita <90214094+E-Verita@users.noreply.github.com>
2022-11-23 20:33:45 +0200

    Docstrings - documentation of functions

--- src/udemy/37 - functions - palindrome.py
+++ src/udemy/37 - functions - palindrome.py
@@ -1,4 +1,16 @@
 def multiply(a, b):
+    """
+Multiply 2 numbers.
+
+    Although this function is intended to multiply 2 numbers,
+    you can also use it to multiply a sequence.  If you pass
+    a string, for example, as the first argument, you'll get
+    the string repeated `y` times as the returned value.
+
+    :param x: The first number to multiply.
+    :param y: The number to multiply `x` by.
+    :return: The product of `x` and `y`.
+"""
     return a * b
 
 
@@ -10,11 +22,28 @@ for val in range(1, 11):
 
 
 def is_palindrome(string):
-    return string[::-1] == string
+    """
+    Check if a string is a palindrome.
+
+    A palindrome is a string that reads the same forwards as backwards.
+
+    :param string: The string to check.
+    :return: True if `string` is a palindrome, False otherwise.
+    """
+    return string[::-1].casefold() == string.casefold()
 
 
-def is_sentence_palindrome(string):
-    string = "".join([el for el in string if el.isalnum() == True])
+def is_sentence_palindrome(sentence):
+    """
+    Check if a sentence is a palindrome.
+
+    The function ignores whitespace, capitalisation and
+    punctuation in the sentence.
+
+    :param sentence: The sentence to check.
+    :return: True if `sentence` is a palindrome, False otherwise.
+    """
+    string = "".join([el for el in sentence if el.isalnum() == True])
     return is_palindrome(string)
 
 
